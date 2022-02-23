e46604fc9de21353e960590c72df6f8cde037f00
dar4kamal <sasaa9646@gmail.com>
2022-02-23 15:25:30 +0200

    updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="h-16 text-xl text-white bg-blue-800">Footer</div>;
+  return (
+    <div className="flex items-center justify-between h-16 text-xl text-white bg-blue-800">
+      <div className="ml-5">
+        Inspirational quotes provided by
+        <a
+          target="_blank"
+          className="external-link"
+          href="https://zenquotes.io/"
+        >
+          ZenQuotes API
+        </a>
+      </div>
+      <div className="mr-5">
+        Wanna Contribute ?{" "}
+        <a
+          target="_blank"
+          className="external-link"
+          href="https://github.com/dar4kamal/highlight-share-chrome-extension"
+        >
+          Github Repo
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
